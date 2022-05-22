2fb7d1c80f8126c80afdbc857f64492a691a93fb
reinaldocs <santosdoscorreiareinaldo@gmail.com>
2022-05-22 01:41:57 -0300

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * CPF = string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: []
+  })
+
+  return response.status(200).json(customers);
+});
 
 app.listen(3333, () => {
   console.log('listening on port: 3333');
