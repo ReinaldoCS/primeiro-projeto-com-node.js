a45876aeedb0aaa67c5717ef4e0f02578de1a91a
reinaldocs <santosdoscorreiareinaldo@gmail.com>
2022-05-23 00:13:45 -0300

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else { 
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+};
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -49,7 +61,7 @@ app.post('/account', (request, response) => {
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
-  response.json(customer.statement);
+  response.status(201).json(customer.statement);
 });
 
 app.listen(3333, () => {
@@ -64,10 +76,31 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     description, 
     amount,
     created_at: new Date(),
-    type: "credit"
+    type: "credit",
   };
 
   customer.statement.push(statementOperation);
 
-  response.json(statementOperation);
+  response.status(201).json(statementOperation);
 })
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  if(amount > balance) {
+    return response.status(400).json({ error: 'Insufficient funds!' });
+  };
+
+  const statementOperation = {
+    amount,
+    description,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  response.status(201).json(statementOperation);
+});
